15fe8c345d0b3b050a3fd8b37882480e3b7ce629
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2023-03-19 13:48:23 +0200

    create windows service UI

--- image_capture.py
+++ image_capture.py
@@ -1,22 +1,34 @@
 import time
 from SMWinservice import SMWinservice
 import cv2
+import sys
 
-class PythonCornerExample(SMWinservice):
-    _svc_name_ = "PythonCornerExample"
+
+class FaceWatchService(SMWinservice):
+    _svc_name_ = "FaceWatchService"
     _svc_display_name_ = "Python Corner's Winservice Example"
     _svc_description_ = "That's a great winservice! :)"
 
-    def start(self):
+    def start(self, argv=("",)):
+        self.map = {
+            "": 1,
+            "15 sec": 15,
+            "15 min": 13 * 60,
+            "30 min": 30 * 60,
+            "1 h": 60 * 60,
+        }
+        arg1 = argv[1] if len(argv) > 1 else ""
+        self.interval = self.map[arg1]
         self.isrunning = True
 
     def stop(self):
         self.isrunning = False
 
     def main(self):
+        self.interval = 15
         while self.isrunning:
             take_photo()
-            time.sleep(1)
+            time.sleep(self.interval)
 
 
 def take_photo():
@@ -25,11 +37,10 @@ def take_photo():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 48)
     ret, frame = cap.read()
     cap.release()
-    filename = f'C:/Users/UG/Desktop/research/FetchWatch/Images/image{time.strftime("%Y%m%d-%H%M%S")}.jpg'
+    filename = f'C:/Users/UG/Desktop/research/FaceWatch/Images/image{time.strftime("%Y%m%d-%H%M%S")}.jpg'
     cv2.imwrite(filename, frame)
-    print(ret)
-    print(frame.shape)
+
 
 if __name__ == "__main__":
-    PythonCornerExample.parse_command_line()
-    #take_photo()
+    FaceWatchService.parse_command_line()
+    # take_photo()
