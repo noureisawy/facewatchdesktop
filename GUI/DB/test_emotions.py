89c47b8cc32d87f758b165d56881d9d8a9062e5e
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2023-04-01 21:59:46 +0200

    test create to test tiredness database

---
+++ GUI/DB/test_emotions.py
@@ -0,0 +1,67 @@
+from Data import Data
+
+
+data = Data()
+# insert some emotional data
+data.insert_emotion(
+    {
+        "emotion": {
+            "angry": 0.0,
+            "disgust": 0.0,
+            "fear": 0.0,
+            "happy": 0.0,
+            "sad": 0.0,
+            "surprise": 0.0,
+            "neutral": 1.0,
+        },
+        "dominant_emotion": "neutral",
+    }
+)
+
+data.insert_emotion(
+    {
+        "emotion": {
+            "angry": 1.0,
+            "disgust": 0.0,
+            "fear": 0.0,
+            "happy": 0.0,
+            "sad": 0.0,
+            "surprise": 0.0,
+            "neutral": 0.0,
+        },
+        "dominant_emotion": "angry",
+    }
+)
+
+data.insert_emotion(
+    {
+        "emotion": {
+            "angry": 0.0,
+            "disgust": 1.0,
+            "fear": 0.0,
+            "happy": 0.0,
+            "sad": 0.0,
+            "surprise": 0.0,
+            "neutral": 0.0,
+        },
+        "dominant_emotion": "disgust",
+    }
+)
+
+data.insert_emotion(
+    {
+        "emotion": {
+            "angry": 0.0,
+            "disgust": 0.0,
+            "fear": 0.0,
+            "happy": 0.0,
+            "sad": 1.0,
+            "surprise": 0.0,
+            "neutral": 0.0,
+        },
+        "dominant_emotion": "sad",
+    }
+)
+
+# get all data
+print(data.get_date_between("2023-03-23 02:18:54", "2023-03-24 02:18:54"))
